336c1061eea3f86f88c17a6e80022f7e8566776f
이아람 <aram.lee5@cj.net>
2023-11-23 17:23:57 +0900

    chart 옵션추가 & 꾸미기 추가작업

--- src/screens/coins/Chart.tsx
+++ src/screens/coins/Chart.tsx
@@ -3,6 +3,7 @@ import { useOutletContext } from "react-router";
 import { fetchCoinHistory } from "../../api";
 import ApexChart from "react-apexcharts";
 import Loading from "../../components/Loading";
+import { useState } from "react";
 interface IHistorical {
   time_open: string;
   time_close: string;
@@ -21,24 +22,55 @@ function Chart() {
   const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  console.log(isLoading, data);
+  console.log(
+    data?.map((price) => price.close) as number[],
+    data?.map((price) => parseFloat(price.time_close)) as number[]
+  );
   return (
     <div>
       <ApexChart
         options={{
           chart: {
             width: 500,
-            height: 500,
+            height: 300,
+            toolbar: {
+              show: false,
+            },
+            background: "transparent",
+          },
+          theme: {
+            mode: "dark",
+            palette: "palette5",
+            monochrome: {
+              enabled: false,
+              color: "#255aee",
+              shadeTo: "light",
+              shadeIntensity: 0.65,
+            },
+          },
+          stroke: {
+            curve: "smooth",
+            width: 3,
+          },
+          grid: {
+            row: {
+              colors: undefined,
+              opacity: 0.5,
+            },
+          },
+          yaxis: {
+            show: false,
+          },
+          xaxis: {
+            axisBorder: { show: false },
+            axisTicks: { show: false },
+            labels: { show: false },
           },
         }}
         series={[
           {
-            name: "series-1",
-            data: [30, 40, 45, 50, 49, 60, 70, 91],
-          },
-          {
-            name: "series-2",
-            data: [50, 5, 24, 50, 23, 60, 14, 120],
+            name: "Price",
+            data: data?.map((price) => price.close) as number[],
           },
         ]}
         type="line"
